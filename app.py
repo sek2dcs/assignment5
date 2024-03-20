429c7bd64e0b95acbf1f5327333f3f766c0fd2a9
Sophie Kim <143015489+sek2dcs@users.noreply.github.com>
2024-03-20 15:39:53 -0400

    debugging for render

--- app.py
+++ app.py
@@ -24,29 +24,27 @@ app.layout = html.Div([
     html.H1(children = "Gapminder - GDP per capita from 1800-2100"),
     html.Div(children = "This dataset is from Gapminder's estimates of GDP per capita for almost all countries in the world from 18000 to 2100. The creators of this dataset were able to make the GDPs of each country on the same 'scale' by standardizing using a World Bank indicator. Depending on the year, Gapminder had different methods on how to estimate the GDP per capita for each country. Additionally, Syria did not have any official GDP per capita estimate since 2010, so Gapminder estimated these values. This app has a multi-select dropdown where the user can select multiple countries, a range slider for the years in the dataset, and a line graph displaying the gdp per captia for each country from 1800-2100."),
     html.Div([
-    html.Div(
         dcc.Dropdown(
             options = [{'label': country, 'value': country} for country in data_fixed.country.unique()], 
-            id = "dropdown-country", 
+            id = 'dropdown-country', 
             multi = True, 
-            value = ['UK', 'Angola']
-        ), className = "col-md-6", 
-    ), 
-    html.Div(
-        dcc.RangeSlider(min = 1800, max = 2100, id = 'range-slide-yr', 
-                        value = [1800, 1900], 
-                        marks = slider_marks,
-                         tooltip = {'placement' : 'bottom', 'always_visible': True}), 
-        className = "col-md-6"
-    )
-], className = "row"),
-html.Div([    
-    dcc.Graph(id = 'line-graph'
-    )
+            value = ['UK', 'Angola'],
+            style = {'width': '48%', 'display': 'inline-block'}
+        ),
+        dcc.RangeSlider(
+            min = 1800, 
+            max = 2100, 
+            id = 'range-slide-yr', 
+            value = [1800, 1900], 
+            marks = slider_marks,
+            tooltip = {'placement': 'bottom', 'always_visible': True},
+            style = {'width': '48%', 'display': 'inline-block', 'float': 'right'}
+        )
+    ]),
+    html.Div([    
+        dcc.Graph(id = 'line-graph')
+    ])
 ])
-])
-
-
 @app.callback(
     Output('line-graph', 'figure'), 
     [Input('dropdown-country', 'value'), 
