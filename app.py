d28370b86623533eeda139f290d9e8b7b21c55b8
Sophie Kim <143015489+sek2dcs@users.noreply.github.com>
2024-03-20 14:59:21 -0400

    got rid of comments bc maybe that's why render not working

--- app.py
+++ app.py
@@ -1,72 +1,62 @@
-# importing packages 
 from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd 
 
-# reading in csv
 data_fixed = pd.read_csv("https://github.com/sek2dcs/assignment5/blob/main/data_fixed.csv", on_bad_lines = 'skip', encoding='utf-8')
 data_fixed = data_fixed.reset_index()
 
-# making filtered df of unique years for slider marks 
-# have to make into integers so that the filtered_years works (to be divisble by 100)
 unique_years = sorted([int(year) for year in data_fixed['year'].unique()])
 
-# then making list of years by 100 by saying that put the year in this list if it is divisible by 100 
 filtered_years = [year for year in unique_years if year % 100 == 0]
 
-# creating dictionary of the diff years 
 slider_marks = {year: str(year) for year in filtered_years}
-
-# initializing app 
+ 
 app = Dash(__name__)
 server = app.server
 
-# building app 
 app.layout = html.Div([
-    html.H1(children = "Gapminder - GDP per capita from 1800-2100"), # title   (below is the paragraph -- in the second div)
+    html.H1(children = "Gapminder - GDP per capita from 1800-2100"),
     html.Div(children = "This dataset is from Gapminder's estimates of GDP per capita for almost all countries in the world from 18000 to 2100. The creators of this dataset were able to make the GDPs of each country on the same 'scale' by standardizing using a World Bank indicator. Depending on the year, Gapminder had different methods on how to estimate the GDP per capita for each country. Additionally, Syria did not have any official GDP per capita estimate since 2010, so Gapminder estimated these values. This app has a multi-select dropdown where the user can select multiple countries, a range slider for the years in the dataset, and a line graph displaying the gdp per captia for each country from 1800-2100."),
     html.Div([
     html.Div(
         dcc.Dropdown(
-            options = [{'label': country, 'value': country} for country in data_fixed.country.unique()], # this gives the dropdown options as the diff countries in the country column of df
+            options = [{'label': country, 'value': country} for country in data_fixed.country.unique()], 
             id = "dropdown-country", 
             multi = True, 
             value = ['UK', 'Angola']
-        ), className = "six columns",  # makes it so it takes up half (6/12)
+        ), className = "six columns", 
     ), 
     html.Div(
         dcc.RangeSlider(min = 1800, max = 2100, id = 'range-slide-yr', 
                         value = [1800, 1900], 
-                        marks = slider_marks, # markers for every 100 yrs 
-                         tooltip = {'placement' : 'bottom', 'always_visible': True}), # makes it so that the user can see the specific year selected on slider range
-        className = "six columns" # makes it so it takes up half (6/12)
+                        marks = slider_marks,
+                         tooltip = {'placement' : 'bottom', 'always_visible': True}), 
+        className = "six columns" 
     )
-], className = "row"), # puts the slider & dropdown on one row 
-html.Div([      # another parent div for the line graph 
+], className = "row"),
+html.Div([    
     dcc.Graph(id = 'line-graph'
     )
 ])
 ])
 
 
-# defining callback
 @app.callback(
-    Output('line-graph', 'figure'), # outputs will be the line graph 
-    [Input('dropdown-country', 'value'), # inputs are the dropdowns & the range slider 
+    Output('line-graph', 'figure'), 
+    [Input('dropdown-country', 'value'), 
      Input('range-slide-yr', 'value')]
 )
-def update_fig(selected_country, selected_year): # defining function as update_fig
+def update_fig(selected_country, selected_year): 
     dff = data_fixed[(data_fixed['year'] >= selected_year[0]) & (data_fixed['year'] <= selected_year[1]) & (data_fixed['country'].isin(selected_country))]
-    # making dff which filters for the selected year min & max from the slider and the selcted countries from the dropdown ^ 
-    fig = px.line(dff, x='year', # making line graph! 
+    fig = px.line(dff, x='year', 
                   y= 'gdp_per_capita',
                   color = 'country', title = "year vs gdp per capita per selected countries")
-    fig.update_xaxes(title = "year") # titling the axes 
+    fig.update_xaxes(title = "year") 
     fig.update_yaxes(title = 'gdp per capita')
-    fig.update_layout(transition_duration=500) # doing some animation 
+    fig.update_layout(transition_duration=500) 
 
     return fig
 
-# running app
+
 if __name__ == '__main__':
     app.run_server(debug=True)
